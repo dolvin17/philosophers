f62a9582c4ea36d45c8ce9f67a4b2f8e7b4dc028
dolvin17 <huertas876@gmail.com>
2024-01-10 21:28:20 +0100

    testing stuffs

--- philo.h
+++ philo.h
@@ -1,40 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/08 18:30:46 by dolvin17          #+#    #+#             */
-/*   Updated: 2024/01/10 17:42:22 by dolvin17         ###   ########.fr       */
+/*   Created: 2024/01/10 17:25:13 by dolvin17          #+#    #+#             */
+/*   Updated: 2024/01/10 21:27:48 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+#endif
 
-#include "philo.h"
+#include <stdio.h>
+#include <sys/time.h>
 
-
-int	main(int argc, char **argv)
-{
-	int	i;
-	struct timeval start_time;
-	struct timeval end_time;
-	long	time;
-
-
-	i = 0;
-	gettimeofday(&start_time, NULL);
-	if (argc <= 6)
-	{
-		while (i < 1000)
-		{
-			//printf("%s ", argv[i]);
-			i++;
-		}
-	}
-	printf("%s ", argv[i]);
-	gettimeofday(&end_time, &start_time);
-	time = end_time.tv_sec - start_time.tv_sec;
-	printf("%ld", time);
-	return (0);
-}
