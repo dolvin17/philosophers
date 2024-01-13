f498ed0b374aab88a985d9755617fefa9f209a03
dolvin17 <huertas876@gmail.com>
2024-01-13 23:57:33 +0100

    testing phread fn

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 18:30:46 by dolvin17          #+#    #+#             */
-/*   Updated: 2024/01/10 17:42:22 by dolvin17         ###   ########.fr       */
+/*   Updated: 2024/01/13 23:56:55 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	main(int argc, char **argv)
 	struct timeval start_time;
 	struct timeval end_time;
 	long	time;
+	pthread_t thread_id;
+
 
 
 	i = 0;
@@ -34,7 +36,9 @@ int	main(int argc, char **argv)
 	}
 	printf("%s ", argv[i]);
 	gettimeofday(&end_time, &start_time);
-	time = end_time.tv_sec - start_time.tv_sec;
+	time = end_time.tv_sec - start_time.tv_sec; 
 	printf("%ld", time);
+	thread_function();
+	pthread_create(&thread_id, NULL, &thread_function, NULL);
 	return (0);
 }
