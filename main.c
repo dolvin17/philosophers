0b92b53c6b1cda0149db324a2f9ff3dc78a7da56
dolvin17 <huertas876@gmail.com>
2024-01-14 17:59:20 +0100

    testing pthread_create fn

--- main.c
+++ main.c
@@ -6,39 +6,31 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 18:30:46 by dolvin17          #+#    #+#             */
-/*   Updated: 2024/01/13 23:56:55 by dolvin17         ###   ########.fr       */
+/*   Updated: 2024/01/14 17:57:05 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "philo.h"
 
+void	*philo_fn(void *arg)
+{
+	printf("Hello from philo_fn\n");
+	printf("arg = %p\n", arg);
+	return (NULL);
+}
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	struct timeval start_time;
-	struct timeval end_time;
-	long	time;
-	pthread_t thread_id;
+	pthread_t	alex;
+	pthread_t	nick;
 
-
-
-	i = 0;
-	gettimeofday(&start_time, NULL);
+	printf("argv[0] = %s\n", argv[0]);
 	if (argc <= 6)
 	{
-		while (i < 1000)
-		{
-			//printf("%s ", argv[i]);
-			i++;
-		}
+
+		pthread_create(&alex, NULL, &philo_fn, NULL);
+		pthread_create(&nick, NULL, &philo_fn, NULL);
 	}
-	printf("%s ", argv[i]);
-	gettimeofday(&end_time, &start_time);
-	time = end_time.tv_sec - start_time.tv_sec; 
-	printf("%ld", time);
-	thread_function();
-	pthread_create(&thread_id, NULL, &thread_function, NULL);
 	return (0);
 }
